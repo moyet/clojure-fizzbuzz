66a9e0ce13035e5edf5f21f542649ef2590a2803
= <=>
2021-03-18 22:30:20 +0100

    Added early fizzbuzz function

--- src/fizzbuzz/core.clj
+++ src/fizzbuzz/core.clj
@@ -1,6 +1,28 @@
 (ns fizzbuzz.core)
 
-(defn foo
+
+(defn fizzbuzz
   "I don't do a whole lot."
   [x]
-  (println x "Hello, World!"))
+  (let [a (atom "")]
+    (when (= (mod x 3) 0)
+      (swap! a str "Fizz")
+      )
+    (when (= (mod x 5) 0)
+      (swap! a str "Buzz")
+      )
+    (if (empty? @a)
+      (str x)
+      @a
+      )
+    )
+    )
+
+(defn main-funktion
+  "Prints out fizzbuzz "
+  [x]
+  (let [rang (range 1 (inc x))]
+    (map fizzbuzz rang)
+    )
+  )
+
